9f8eaa3fd34ae2b3ab3475dcb7b888b619ac825d
geoport <numanburakfidan@yandex.com>
2022-06-08 13:07:58 +0300

    struct fields exported

--- soil_profile/data_structures.go
+++ soil_profile/data_structures.go
@@ -2,29 +2,29 @@ package soil_profile
 
 // Layer is a struct that contains the properties of single layer of a soil profile
 type Layer struct {
-	soilClass              string
-	soilClassManuel        string
-	soilClassSelected      string
-	soilType               string
-	soilDefinition         string
-	soilDefinitionManuel   string
-	soilDefinitionSelected string
-	materialType           string
-	thickness              float64
-	dryUnitWeight          float64
-	saturatedUnitWeight    float64
-	fineContent            float64
-	liquidLimit            float64
-	plasticLimit           float64
-	plasticityIndex        float64
+	SoilClass              string
+	SoilClassManuel        string
+	SoilClassSelected      string
+	SoilType               string
+	SoilDefinition         string
+	SoilDefinitionManuel   string
+	SoilDefinitionSelected string
+	MaterialType           string
+	Thickness              float64
+	DryUnitWeight          float64
+	SaturatedUnitWeight    float64
+	FineContent            float64
+	LiquidLimit            float64
+	PlasticLimit           float64
+	PlasticityIndex        float64
 	Cu                     float64
-	cohesion               float64
-	phi                    float64
-	waterContent           float64
-	poissonRatio           float64
-	elasticModulus         float64
-	shearModulus           float64
-	voidRatio              float64
+	Cohesion               float64
+	Phi                    float64
+	WaterContent           float64
+	PoissonRatio           float64
+	ElasticModulus         float64
+	ShearModulus           float64
+	VoidRatio              float64
 	Cr                     float64
 	Cc                     float64
 	Gp                     float64
@@ -32,15 +32,15 @@ type Layer struct {
 	VS                     float64
 	RQD                    float64
 	IS50                   float64
-	kp                     float64
-	dampingRatio           float64
+	Lp                     float64
+	DampingRatio           float64
 }
 
 // SoilProfile is a struct that contains the properties of a soil profile
 type SoilProfile struct {
 	Layers       []Layer
-	gwt          float64
-	checkGwt     bool
-	densityUnit  string
-	pressureUnit string
+	Gwt          float64
+	CheckGwt     bool
+	DensityUnit  string
+	PressureUnit string
 }
